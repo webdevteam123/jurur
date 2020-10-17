a7f8227cdfc8205f8581d2e757bbe68b615c4e4d
tanner-tech <dotdottanner@gmail.com>
2020-10-17 12:43:10 +0800

    payment page first commit and other little change commits

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const header = document.querySelector("header");
 let isChanged = false;
 window.addEventListener("scroll", e => {
-    console.log(window.scrollY, isChanged);
     if(window.scrollY > 100 && !isChanged) {
         header.classList.add("white");
         header.classList.remove("trans");
