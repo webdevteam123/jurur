ba33493fcce468215d728bfc22fe7ea56f8cc999
tanner-tech <dotdottanner@gmail.com>
2020-10-29 20:57:59 +0800

    product  page chagen commit

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const header = document.querySelector("header");
 let isChanged = false;
 window.addEventListener("scroll", e => {
-    if(window.scrollY > 100 && !isChanged) {
+    if(window.scrollY > 80 && !isChanged) {
         header.classList.add("white");
         header.classList.remove("trans");
         isChanged = true;
     }
-    if(window.scrollY <= 100 && isChanged) {
+    if(window.scrollY <= 80 && isChanged) {
         header.classList.add("trans");
         header.classList.remove("white");
         isChanged = false;
